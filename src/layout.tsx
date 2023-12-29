f506dc5596bacd616cd5006a917db40e104a3d1a
swikars1 <swikarsharma@gmail.com>
2023-12-30 01:28:52 +0545

    add hyperscript

--- src/layout.tsx
+++ src/layout.tsx
@@ -5,7 +5,13 @@ export const Layout = ({ children }: { children: JSX.Element }) => {
     <html lang="en">
       <head>
         <title>ChatMX App</title>
+        <script src="https://cdn.jsdelivr.net/npm/@unocss/runtime"></script>
         <script src="/public/htmx.min.js"></script>
+        <script src="https://unpkg.com/hyperscript.org@0.9.12"></script>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@unocss/reset/tailwind.min.css"
+        />
       </head>
       <body>
         <h1>ChatMX</h1>
