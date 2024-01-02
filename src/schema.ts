f882a870df0bb2311109c9e8c4ad033f6e1f7554
swikars1 <swikarsharma@gmail.com>
2024-01-03 01:37:26 +0545

    remove unwanted file

--- src/schema.ts
+++
@@ -1,7 +0,0 @@
-import { sql } from "drizzle-orm/sql";
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
-
-export const users = sqliteTable("users", {
-  id: text("id"),
-  name: text("name"),
-});
